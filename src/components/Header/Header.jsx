5adb7f8410c75e1f8ba643d15c1c99765ed7a2c8
Caio Neves <ncaio037@gmail.com>
2023-06-18 20:04:45 -0300

    added transition in home component and styles to modal.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,38 +1,55 @@
 import style from './Header.module.scss'
 import svgBuble from '../../assets/images/blob.png'
 import Modal from '../Modal/Modal'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { useSpring, animated } from '@react-spring/web'
+
+
 
 const Header = () => {
 
     const [open, setOpen] = useState(false)
+    const [showContent, setShowContent] = useState(false);
+  
+    const headerIn = useSpring({
+        opacity: showContent ? 1 : 0,
+        marginLeft: showContent ? 0 : -100,
+        delay: 340,
+
+    });
+
+    useEffect(() => {
+        setShowContent(true);
+    }, []);
 
     return(
         <>
-            <header className={style.header}>
+            <animated.div style={headerIn}>
+                <header className={style.header}>
 
-                <div className={style.apresentation}>
+                    <div className={style.apresentation}>
 
-                    <img src={svgBuble} alt="" className={style.imgBuble}/>
+                        <img src={svgBuble} alt="" className={style.imgBuble}/>
 
-                    <div className={style.description}>
-                        <h1>Descubra o sabor do mundo inteiro com apenas um clique!</h1>
-                        <h2>Bem-vindo ao Saveur Divine, o destino gastronômico que irá transportar você para um mundo de delícias culinárias. Se você está em busca de uma experiência única e inesquecível, você encontrou o lugar certo!</h2>
+                        <div className={style.description}>
+                            <h1>Descubra o sabor do mundo inteiro com apenas um clique!</h1>
+                            <h2>Bem-vindo ao Saveur Divine, o destino gastronômico que irá transportar você para um mundo de delícias culinárias. Se você está em busca de uma experiência única e inesquecível, você encontrou o lugar certo!</h2>
+                        </div>
+
                     </div>
 
-                </div>
+                    <div className={style.box}>
 
-                <div className={style.box}>
+                        <div className={style.boxTitle}>
+                            <h1>Cuisine</h1>
+                            <h1>Du Monde</h1>
+                        </div>
 
-                    <div className={style.boxTitle}>
-                        <h1>Cuisine</h1>
-                        <h1>Du Monde</h1>
+                        <button onClick={() => setOpen(!open)}>Reservation</button>
                     </div>
-
-                    <button onClick={() => setOpen(!open)}>Reservation</button>
-                </div>
-            </header>
-            <Modal isOpen={open} actionName='reservation' setOpen={setOpen}/>
+                </header>
+                <Modal isOpen={open} actionName='reservation' setOpen={setOpen}/>
+            </animated.div>
         </>
     )
 }
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,14 +1,37 @@
 import style from './Modal.module.scss'
+import reservationSvg from'../../assets/images/reservation.svg'
 
 const Modal = ({ isOpen, actionName, setOpen }) => {
 
-    if(isOpen) {
+    if(isOpen && actionName === 'reservation') {
         return(
             <div className={style.background}>
                 <div className={style.modal}>
-                    <h2>Make your {actionName} right now!</h2>
-                    <p>Description</p>
-                    <button onClick={() => setOpen(!isOpen)}>Close</button>
+                    
+                    <div className={style.apresentationModal}>
+                        <img src={reservationSvg} alt="" className={style.modalSvg}/>
+                        <button onClick={() => setOpen(!isOpen)}>Close Modal</button>
+                    </div>
+
+                    <form action="" className={style.form}>
+                        <h2>Make your {actionName} right now!</h2>
+
+                        <label htmlFor="">Email</label>
+                        <input type="email" placeholder='you@example.com'/>
+            
+                        <label htmlFor="">Password</label>
+                        <input type="password" placeholder='At least 8 characters'/>
+
+                        <label htmlFor="">Date</label>
+                        <input type="date"/>
+
+                        <label htmlFor="">Time</label>
+                        <input type="time"/>
+
+                        <button type='submit'>Send</button>
+                    </form>
+
+                    
                 </div>
             </div>
         )
